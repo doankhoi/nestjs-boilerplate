0f012c915abb50939670a2394b898c7ba1a1e2df
doankhoi <doanngockhoi93@gmail.com>
2022-01-20 22:25:05 +0700

    feat: ✨ api join, approve campaign

---
+++ src/repositories/campaign-member.repository.ts
@@ -0,0 +1,15 @@
+import { CampaignMember } from '@entities';
+import { BaseRepository } from './base.repository';
+
+export class CampaignMemberRepository extends BaseRepository<CampaignMember> {
+  constructor() {
+    super(CampaignMember);
+  }
+
+  findMember(userId: string, campaignId: string) {
+    return this.getRepository().findOne({
+      campaignId,
+      userId,
+    });
+  }
+}
---
+++ src/resolvers/campain-member.resolver.ts
@@ -0,0 +1,82 @@
+import { AuthGuard, CampaignMemberStatus, CurrentUser } from '@common';
+import { ApproveMemberDto } from '@dtos';
+import { CampaignMember, User } from '@entities';
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CampaignMemberService, CampaignService } from '@services';
+import { UserInputError, ForbiddenError } from 'apollo-server-core';
+
+@Resolver(() => User)
+export class CampaignMemberResolver {
+  constructor(
+    private campaignMemberService: CampaignMemberService,
+    private campaignService: CampaignService,
+  ) {}
+
+  @Mutation(() => CampaignMember)
+  @UseGuards(AuthGuard)
+  async joinCampaign(
+    @Args('campaignId') campaignId: string,
+    @CurrentUser() user: User | null,
+  ): Promise<CampaignMember> {
+    if (!user) {
+      throw new UserInputError('Not found wallet address');
+    }
+    const userId = user._id.toString();
+    const campaign = await this.campaignService.campaignRepository.findById(
+      campaignId,
+    );
+
+    if (!campaign) {
+      throw new UserInputError('Not found the campaign');
+    }
+
+    let campaignMember =
+      await this.campaignMemberService.campaignMemberRepository.findMember(
+        userId,
+        campaignId,
+      );
+    if (!campaignMember) {
+      campaignMember =
+        await this.campaignMemberService.campaignMemberRepository.create(
+          new CampaignMember({ userId, campaignId }),
+        );
+    }
+    return campaignMember;
+  }
+
+  @Mutation(() => CampaignMember)
+  @UseGuards(AuthGuard)
+  async approvedMember(
+    @Args('input') input: ApproveMemberDto,
+    @CurrentUser() user: User | null,
+  ): Promise<CampaignMember> {
+    if (!user) {
+      throw new UserInputError('User not register');
+    }
+    const requestUserId = user._id.toString();
+
+    const { userId, campaignId } = input;
+    const campaign = await this.campaignService.campaignRepository.findById(
+      campaignId,
+    );
+    if (!campaign) {
+      throw new UserInputError('Not found the campaign');
+    }
+
+    if (campaign.creatorId !== requestUserId) {
+      throw new ForbiddenError('You dont have permission');
+    }
+
+    const campaignMember =
+      await this.campaignMemberService.campaignMemberRepository.findMember(
+        userId,
+        campaignId,
+      );
+    if (!campaignMember) {
+      throw new UserInputError('User not in campaign');
+    }
+    campaignMember.status = CampaignMemberStatus.APPROVED;
+    return campaignMember;
+  }
+}
