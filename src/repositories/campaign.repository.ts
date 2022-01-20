27e8e28bd48bb7778f521616709c7047cead6bc5
doankhoi <doanngockhoi93@gmail.com>
2022-01-20 10:10:35 +0700

    feat: ⚡ add resolve fields in campaigns

--- src/repositories/campaign.repository.ts
+++ src/repositories/campaign.repository.ts
@@ -5,4 +5,12 @@ export class CampaignRepository extends BaseRepository<Campaign> {
   constructor() {
     super(Campaign);
   }
+
+  findCampaignsByUserId(userId: string, limit = 20, offset = 0) {
+    return this.getRepository().findAndCount({
+      where: { creatorId: userId },
+      take: limit,
+      skip: offset,
+    });
+  }
 }
